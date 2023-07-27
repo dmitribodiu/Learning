daacb3e3ab23cd6508f892e69bafc2abd0085b75
Dmitri Bodiu <dmitribodiu1989@gmail.com>
2023-07-27 20:16:34 +0300

    fdfsd

--- Src/Demo/Program.cs
+++ Src/Demo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace Demo
 {
@@ -6,10 +8,71 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            int[] numbers = new[] { 23, 1, 3, 5, 6, 6, 8 };
+
+            var result = numbers.MaxNumber();
 
 
-            Console.WriteLine("Hello World!");
+            string[] words = new[] { "one", "two"};
+
+            List<int> numbers2 = new List<int> {  };
+            numbers2.Add(3);
+            numbers2.AddRange(numbers);
+
+            string question = "How1";
+            bool length = question.Oleg(3);
+            
+            
+            double price = 34.64;
+            char letter = 's';
+            DateTime asss = DateTime.Now;
+
+
+            Console.WriteLine(question);
+            string answer = Console.ReadLine();
+            int answerAsInt = int.Parse(answer);
+
+            bool a = answerAsInt > 30;
+            if (a)
+            {
+                Console.WriteLine("Old");
+            }
+            else
+            {
+                Console.WriteLine("Young");
+            }
+
             Console.ReadKey();
+        }
+    }
+
+    public static class Utils
+    {
+        public static bool Oleg(this string text, int param)
+        {
+            if (param == text.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int MaxNumber(this int[] array)
+        {
+            int maxNumber = Int32.MinValue;
+
+            foreach (int number in array)
+            {
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+
+            return maxNumber;
         }
     }
 }
