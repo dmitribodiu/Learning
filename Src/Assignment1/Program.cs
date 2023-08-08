ff61bde4336bea720ced7000a2273f71f93671a7
Dmitri Bodiu <dmitribodiu1989@gmail.com>
2023-08-08 20:47:18 +0300

    BinarySearch

--- Src/Assignment1/Program.cs
+++ Src/Assignment1/Program.cs
@@ -11,9 +11,36 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
+            var numberToSearch = 5;
+            var array = new int[] { 5, 7, 9, 12, 24, 30, 44, 56, 82 };
+
+            var found = BinarySearch(array, numberToSearch, 0, array.Length - 1);
+
+            if (found)
+            {
+                Console.WriteLine($"Yeeah! Number {numberToSearch} was found");
+            }
+
+            // 5! = 1 * 2 * 3 * 4 * 5;
+
+            var rs = FactorialNonRecursive(5);
+            var result = Factorial(5);
+            int sum = 0;
+            int counter = 1;
+            int ten = 10;
+
+            while (counter < ten)
+            {
+                sum = sum + counter;
+                counter++;
+            }
+
+            Console.WriteLine(sum);
+
+
             var avg = (3 + 2 + 1) / 3;
-            var array = new double[] { 3, 2, 1, 2};
-            var avg2 = Enumerable.Average(array);
+            var array1 = new double[] { 3, 2, 1, 2};
+            var avg2 = Enumerable.Average(array1);
 
             var template = $"jfkldsjfdl , fdsfd {avg},,,,, {avg2 * avg2}";
             Task1();
@@ -55,6 +82,69 @@ namespace CSLight
             }
             Console.WriteLine("Done!");
             Console.ReadKey();
+        }
+
+        public static double Factorial(int number)
+        {
+            if (number == 0)
+                return 1;
+            var result =  number * Factorial(number - 1);
+            return result;
+        }
+
+        public static double FactorialNonRecursive(int number)
+        {
+            if (number == 0)
+                return 1;
+
+            double factorial = 1;
+            for (int i = number; i >= 1; i--)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+
+
+        public static bool BinarySearch(int[] array, int numberToSearch, int min, int max)
+        {
+            if (min == max)
+            {
+                var value = array[min];
+                var result = value == numberToSearch;
+                return result;
+            }
+
+            var mediumIndex = (max + 1) / 2;
+            var mediumValue = array[mediumIndex];
+
+            if (mediumValue > numberToSearch)
+            {
+                var result =  BinarySearch(array, numberToSearch, min, max = mediumIndex - 1);
+                return result;
+            }
+            else if (mediumValue < numberToSearch)
+            {
+                var result = BinarySearch(array, numberToSearch, min = mediumIndex + 1, max);
+                return result;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool BinarySearchNonRecursive(int[] array, int numberToSearch, int min, int max)
+        {
+            return true;
+        }
+    }
+
+    public static class Utils 
+    {
+        public static bool BinarySearch(int[] array, int numberToSearch)
+        {
+            return true;
         }
     }
 }
